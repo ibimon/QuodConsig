7085e1818da3535d107faa0348d0b63e3444b7a9
IBIMON PEREIRA MORAIS <ibimon@gmail.com>
2018-08-02 13:31:25 -0300

    Adicionar arquivos de projeto.

---
+++ QuodConsig.Service/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace QuodConsig.Service.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
